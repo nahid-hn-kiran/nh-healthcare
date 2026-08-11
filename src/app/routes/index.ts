0d860b17ab2ded0ce6f1531decf73d878a1f9a41
nahid7g <nahid.h.kiran@gmail.com>
2026-08-11 11:20:25 +0600

    added: patient and prescription

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,17 +8,21 @@ import { superAdminRoutes } from "../module/superadmin/superadmin.route";
 import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
 import { AppointmentRoutes } from "../module/appointment/appointment.route";
+import { PatientRoutes } from "../module/patient/patient.route";
+import { ReviewRoutes } from "../module/review/review.route";
 
 const router = Router();
 
 router.use("/specialty", specialtyRoutes);
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/patiets", PatientRoutes);
 router.use("/doctor", doctorRoutes);
 router.use("/admin", adminRoutes);
 router.use("/super-admin", superAdminRoutes);
 router.use("/schedule", scheduleRoutes);
 router.use("/doctor-schedule", DoctorScheduleRoutes);
 router.use("/appointments", AppointmentRoutes);
+router.use("/reviews", ReviewRoutes);
 
 export const indexRoutes = router;
